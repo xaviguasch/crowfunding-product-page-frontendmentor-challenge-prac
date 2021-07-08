8ea053dac5e2ce12b933c0c6333bee9ac02bb5a6
xaviguasch <xaviguasch@gmail.com>
2021-07-08 22:56:19 +0200

    added checkbox to select the pledge

--- index.js
+++ index.js
@@ -1,25 +1,62 @@
 const backBtn = document.querySelector('#back-btn')
 const closeModalBtn = document.querySelector('#close-modal-btn')
 const overlay = document.querySelector('#overlay')
-const currMoney = document.querySelector('#current-money-raised')
-const goalMoney = document.querySelector('#goal-money')
-const numBackers = document.querySelector('#num-backers')
-const bambooUnitsLeft = document.querySelector('#bamboo-units-left')
-const blEdUnitsLeft = document.querySelector('#black-edition-units-left')
+
+const currMoneySel = document.querySelector('#current-money-raised')
+const goalMoneySel = document.querySelector('#goal-money')
+const numBackersSel = document.querySelector('#num-backers')
+const bambooUnitsLeftSel = document.querySelector('#bamboo-units-left')
+const blEdUnitsLeftSel = document.querySelector('#black-edition-units-left')
+
+const bambooRewardBtn = document.querySelector('#bamboo-reward-btn')
+const blackEdRewardBtn = document.querySelector('#black-ed-reward-btn')
 
 const progressFill = document.querySelector('.progress-fill')
 
-backBtn.addEventListener('click', () => {
+const noRewardCheckbox = document.querySelector('#no-reward-checkbox')
+
+// Starting numbers and $usd
+const backers = 5007
+const currMoney = 89914
+
+const showModal = () => {
   overlay.style.display = 'block'
-})
+}
+
+backBtn.addEventListener('click', showModal)
+bambooRewardBtn.addEventListener('click', showModal)
+blackEdRewardBtn.addEventListener('click', showModal)
 
 closeModalBtn.addEventListener('click', () => {
   overlay.style.display = 'none'
 })
 
 // Progress bar
-const currMoneyNum = parseFloat(currMoney.textContent.replace(',', ''))
-const goalMoneyNum = parseFloat(goalMoney.textContent.replace(',', ''))
-const progressBarPercentage = (currMoneyNum / goalMoneyNum) * 100
+const fillProgressBar = () => {
+  const currMoneyNum = parseFloat(currMoneySel.textContent.replace(',', ''))
+  const goalMoneyNum = parseFloat(goalMoneySel.textContent.replace(',', ''))
+  const progressBarPercentage = (currMoneyNum / goalMoneyNum) * 100
 
-progressFill.style.width = `${progressBarPercentage}%`
+  progressFill.style.width = `${progressBarPercentage}%`
+}
+
+fillProgressBar()
+
+noRewardCheckbox.addEventListener('change', (e) => {
+  if (noRewardCheckbox.checked) {
+    e.target.parentElement.parentElement.classList.add('selected')
+  } else {
+    e.target.parentElement.parentElement.classList.remove('selected')
+  }
+})
+
+const updateBackers = () => {
+  backers++
+  numBackersSel.textContent = backers
+}
+
+const initialize = () => {
+  numBackersSel.textContent = backers.toLocaleString('en-US')
+}
+
+initialize()
